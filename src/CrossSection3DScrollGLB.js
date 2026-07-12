23dbedf8931db10f18e69d2366a87ee1e2daaf13
playwithlifenow-eng <playwithlifenow@gmail.com>
2026-07-12 05:49:36 +0100

    Update modelPath to use new GLB file name

--- src/CrossSection3DScrollGLB.js
+++ src/CrossSection3DScrollGLB.js
@@ -2505,7 +2505,7 @@ const defaultProps = {
   glassStagger: [0, 0.6],
   oledStagger: [0.15, 0.75],
   phoneStagger: [0.3, 0.9],
-  modelPath: "/14 Pro Model_repaired_v1.glb",
+  modelPath: "/14 pro.glb",
   screenTexture: screenImg,
   internalsTexture: internalsImg,
 };
